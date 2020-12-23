b4a34b52c34c2ea4924bcd240e6f281f9b73391f
axetroy <axetroy.dev@gmail.com>
2020-12-23 20:11:39 +0800

    init

---
+++ README.md
@@ -0,0 +1,37 @@
+# cross-env
+
+![ci](https://github.com/axetroy/cross-env/workflows/ci/badge.svg)
+![Latest Version](https://img.shields.io/github/v/release/axetroy/cross-env.svg)
+![License](https://img.shields.io/github/license/axetroy/cross-env.svg)
+![Repo Size](https://img.shields.io/github/repo-size/axetroy/cross-env.svg)
+
+cross-platform cli for setting environment variables written in [vlang](https://github.com/vlang/v)
+
+## Installation
+
+If you are using `unix` style system(`macOS`/`Linux`). enter the command line to install.
+
+```shell
+# install the latest version
+curl -fsSL https://raw.githubusercontent.com/axetroy/cross-env/main/install.sh | bash
+# install the specified version
+curl -fsSL https://raw.githubusercontent.com/axetroy/cross-env/main/install.sh | bash -s v0.1.0
+```
+
+Or download [the release file](https://github.com/axetroy/cross-env/releases) for your platform and put it to `$PATH` folder.
+
+## Usage
+
+```sh
+$ cross-env FOO=BAR NODE_ENV=PRODUCTION node index.js
+```
+
+## Build from source
+
+```sh
+$ make
+```
+
+## LICENSE
+
+The [MIT License](LICENSE)
---
+++ main.v
@@ -0,0 +1,53 @@
+module main
+
+import os
+import regex
+
+const (
+	version = 'v0.1.0'
+)
+
+fn print_help() {
+	print('cross-env - cross-platform cli for setting environment variables
+
+USAGE:
+  cross-env [Environment variable] <COMMANDS>
+
+EXAMPLE:
+  cross-env FOO=BAR NODE_ENV=PRODUCTION node index.js
+
+SOURCE CODE:
+  https://github.com/axetroy/cross-env
+')
+}
+
+fn main() {
+	args := os.args[1..]
+	query := r'([a-zA-z0-9_]+)=([a-zA-z0-9_]+)'
+	mut reg := regex.regex_opt(query) or { panic(err) }
+	mut env := os.environ()
+	mut index := 0
+	for {
+		if index >= args.len {
+			break
+		}
+		text := args[index]
+		mut start, _ := reg.match_string(text)
+		if start >= 0 && reg.groups.len > 0 {
+			key := text.substr(reg.groups[0], reg.groups[1])
+			value := text.substr(reg.groups[2], reg.groups[3])
+			env[key] = value
+		} else {
+			break
+		}
+		index++
+	}
+	bin_path := os.find_abs_path_of_executable(args[index]) or { panic(err) }
+	mut ps := os.new_process(bin_path)
+	ps.set_args(args[(index + 1)..])
+	ps.set_environment(env)
+	ps.run()
+	ps.wait() // wait for the child process to complete
+	assert ps.pid > 0
+	assert ps.pid != os.getpid()
+}
