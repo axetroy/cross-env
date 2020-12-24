18226a36d154f90e872c184c032e48c094fa6d41
axetroy <axetroy.dev@gmail.com>
2020-12-25 01:13:45 +0800

    update

--- install.sh
+++ install.sh
@@ -5,44 +5,57 @@ downloadFolder="${HOME}/Downloads"
 
 mkdir -p ${downloadFolder}
 
+get_arch() {
+    a=$(uname -m)
+    case ${a} in
+    "x86_64" | "amd64" )
+        echo "amd64"
+        ;;
+    "i386" | "i486" | "i586")
+        echo "386"
+        ;;
+    *)
+        echo ${NIL}
+        ;;
+    esac
+}
+
 get_os(){
     echo $(uname -s | awk '{print tolower($0)}')
 }
 
 main() {
     local os=$(get_os)
-    
-    if [[ $os = "darwin" ]]; then
-        os="osx"
-    fi
-    
-    local dest_file="${downloadFolder}/cross-env_${os}.tar.gz"
-    
+    local arch=$(get_arch)
+    local dest_file="${downloadFolder}/cross-env_${os}_${arch}.tar.gz"
+
     if [[ $# -eq 0 ]]; then
         asset_path=$(
             command curl -sSf https://github.com/axetroy/cross-env/releases |
-            command grep -o "/axetroy/cross-env/releases/download/.*/cross-env_${os}\\.tar.gz" |
-            command head -n 1
+                command grep -o "/axetroy/cross-env/releases/download/.*/cross-env_${os}_${arch}\\.tar.gz" |
+                command head -n 1
         )
         if [[ ! "$asset_path" ]]; then exit 1; fi
         asset_uri="https://github.com${asset_path}"
     else
-        asset_uri="https://github.com/axetroy/cross-env/releases/download/${1}/cross-env_${os}\\.tar.gz"
+        asset_uri="https://github.com/axetroy/cross-env/releases/download/${1}/cross-env_${os}_${arch}\\.tar.gz"
     fi
-    
+
     mkdir -p ${downloadFolder}
-    
+
     echo "[1/3] Download ${asset_uri} to ${downloadFolder}"
     rm -f ${dest_file}
     # wget -P "${downloadFolder}" "${asset_uri}"
     curl --location --output "${dest_file}" "${asset_uri}"
-    
-    echo "[2/3] Install cross-env to the ${HOME}/bin"
+
+    binDir=/usr/local/bin
+
+    echo "[2/3] Install cross-env to the ${binDir}"
     mkdir -p ${HOME}/bin
-    tar -xz -f ${dest_file} -C ${HOME}/bin
-    exe=${HOME}/bin/cross-env
+    tar -xz -f ${dest_file} -C ${binDir}
+    exe=${binDir}/cross-env
     chmod +x ${exe}
-    
+
     echo "[3/3] Set environment variables"
     echo "cross-env was installed successfully to ${exe}"
     if command -v cross-env --version >/dev/null; then
@@ -52,7 +65,7 @@ main() {
         echo "  export PATH=${HOME}/bin:\$PATH"
         echo "Run '$exe --help' to get started"
     fi
-    
+
     exit 0
 }
 
